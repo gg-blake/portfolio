4be7f7d8f909f5340a24edc817f4bd4aeceec08d
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2023-07-09 01:17:21 -0400

    splash page mobile edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": {
+          "50": 'hsl(var(--color-primary-50))',
+          "100": "hsl(var(--color-primary-100))",
+          "200": "hsl(var(--color-primary-200))",
+          "300": "hsl(var(--color-primary-300))",
+          "400": "hsl(var(--color-primary-400))",
+          "500": "hsl(var(--color-primary-500))",
+          "600": "hsl(var(--color-primary-600))",
+          "700": "hsl(var(--color-primary-700))",
+          "800": "hsl(var(--color-primary-800))",
+          "900": "hsl(var(--color-primary-900))"
+        },
+      },
+    },
   },
   plugins: [],
 }
