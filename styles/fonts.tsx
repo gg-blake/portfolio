5a24e33f98b84f8d72dedc08a13c5077c7686a06
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2024-07-25 16:16:52 -0400

    temp deletion

--- styles/fonts.tsx
+++
@@ -1,8 +0,0 @@
-import { Plus_Jakarta_Sans } from 'next/font/google';
-
-const plusJakartaSans = Plus_Jakarta_Sans({
-    weight: ["200", "300", "400", "500", "600", "700", "800"],
-    subsets: ["latin"]
-});
-
-export default plusJakartaSans;
