6c030734c1ff9fc0e3634ffef70a75d20b58b33c
gg-blake <blake092202@gmail.com>
2024-09-13 10:59:49 -0400

    remove comments

--- src/app/api/files/resume/route.js
+++ src/app/api/files/resume/route.js
@@ -5,8 +5,6 @@ export async function GET() {
   const fileId = '1p9YtlJf_vUXefmEoq6APramUdD6JW8Lf4TzY6TKf4QM';
   // The URL to export the document as PDF https://drive.google.com/file/d/1p9YtlJf_vUXefmEoq6APramUdD6JW8Lf4TzY6TKf4QM/view?usp=sharing
   const key = 'AIzaSyBxeyGkT1vdRkw0KFD6Gr80F7qp56H-f2c';
-  // https://www.googleapis.com/drive/v3/files/1p9YtlJf_vUXefmEoq6APramUdD6JW8Lf4TzY6TKf4QM/export?mimeType=application/pdf&key=AIzaSyBxeyGkT1vdRkw0KFD6Gr80F7qp56H-f2c
-  // https://docs.google.com/document/d/1p9YtlJf_vUXefmEoq6APramUdD6JW8Lf4TzY6TKf4QM/edit?usp=sharing
   const exportUrl = `https://www.googleapis.com/drive/v3/files/${fileId}/export?mimeType=application/pdf&key=${key}`;
 
   try {
